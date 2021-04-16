71ef22ffc9378fa8d757a96fa8bdeddd7ceddfc7
Christian Lussier <lussierc@allegheny.edu>
2021-04-16 13:12:18 -0400

    Fix errors in scraping

--- src/scraper.py
+++ src/scraper.py
@@ -7,9 +7,11 @@ year = "2019"
 season_type = "02"
 max_game_ID = 1290
 
-
+print("SCRAPING YOUR DATA")
 # Loop over the counter and format the API call
 for i in range(0, max_game_ID):
+
+    # TODO: add progres bar for scraping
     r = requests.get(
         url="http://statsapi.web.nhl.com/api/v1/game/"
         + year
@@ -22,18 +24,7 @@ for i in range(0, max_game_ID):
 
 print("DATA HAS BEEN SCRAPED")
 
+print("SAVING YOUR DATA")
 
-# Loop over the counter and format the API call
-for i in range(0, max_game_ID):
-    r = requests.get(
-        url="http://statsapi.web.nhl.com/api/v1/game/"
-        + year
-        + season_type
-        + str(i).zfill(4)
-        + "/feed/live"
-    )
-    data = r.json()
-    game_data.append(data)
-
-with open("./" + year + "FullDataset.pkl", "wb") as f:
+with open("./" + year + "my_dataset.pkl", "wb") as f:
     pickle.dump(game_data, f, pickle.HIGHEST_PROTOCOL)
