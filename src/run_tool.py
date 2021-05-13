a69c6911234e795d174daafb3e80e2c4429d5cd6
Christian Lussier <lussierc@allegheny.edu>
2021-05-13 19:05:43 -0400

    Connect scraper and data cleaner

--- src/run_tool.py
+++ src/run_tool.py
@@ -1,4 +1,15 @@
 """Runs the various components of the tool."""
 
+import scraper, data_cleaner
+
 def main():
     """Runs the project."""
+
+    print("Welcome to the NHL Statistics Projection Tool:")
+    input_dec = input("* Do you want to scrape data? Y/N: ")
+
+    if input_dec.upper() == 'Y':
+        scraper.run_scraper()
+        data_cleaner.main()
+    else:
+        data_cleaner.main()
