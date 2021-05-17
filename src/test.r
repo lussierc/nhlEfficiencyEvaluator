299e008e1208a06cd93152fc43e35393b02b7886
Christian Lussier <lussierc@allegheny.edu>
2021-05-17 17:41:17 -0400

    Update the stepwise regression code

--- src/test.r
+++ src/test.r
@@ -2,13 +2,17 @@ head(mtcars)
 head(formatted.nhl.stats)
 
 #define intercept-only model
-intercept_only <- lm(data=formatted.nhl.stats)
+#formatted.nhl.stats
+#> formatted_nhl_full_stats <- read.csv("~/cs590s2021/nhlStatisticProjections/src/formatted_nhl_full_stats.csv", row.names=1)
+#>   View(formatted_nhl_full_stats)
+
+intercept_only <- lm(PTS ~ 1, data=formatted_nhl_full_stats)
 
 #define model with all predictors
-all <- lm(data=formatted.nhl.stats)
+all <- lm(PTS ~ .,data=formatted_nhl_full_stats)
 
 #perform forward stepwise regression
-forward <- step(intercept_only, direction='both', scope=formula(all), trace=0)
+forward <- step(intercept_only, direction='forward', scope=formula(all), trace=0)
 
 #view results of forward stepwise regression
 forward$anova
